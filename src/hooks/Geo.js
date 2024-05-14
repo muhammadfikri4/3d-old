be8bb398f9528bc92357d4c0793eae86cb42e20b
rajifmahendra10 <rajifmahendra10@gmail.com>
2024-05-15 00:35:26 +0700

    update data json

--- src/hooks/Geo.js
+++ src/hooks/Geo.js
@@ -5,9 +5,12 @@ export const useGeo = () => {
     "pk.eyJ1IjoicmFqaWZtYWhlbmRyYSIsImEiOiJjbHVjYTI2d2MwcnBzMmxxbndnMnNlNTUyIn0.aaCGYQ2OYIcIsAa4X-ILDA";
   const map = new mapboxgl.Map({
     // Choose from Mapbox's core styles, or make your own style with Mapbox Studio
-    style: "mapbox://styles/mapbox/light-v11",
-    center: [103.85266553640567, 1.2841309586464738],
-    zoom: 15.5,
+    style: "mapbox://styles/mapbox/outdoors-v12",
+    center: [
+      103.85233599700446,
+      1.284132369245514
+    ],
+    zoom: 20,
     pitch: 45,
     bearing: -17.6,
     container: "map",
@@ -79,20 +82,34 @@ export const useGeo = () => {
     map.addLayer({
       id: "room-extrusion",
       type: "fill-extrusion",
+      //type: "line",
       source: "floorplan",
       paint: {
         // Get the `fill-extrusion-color` from the source `color` property.
-        "fill-extrusion-color": ["get", "color"],
-
+        //"fill-extrusion-color": ["get", "color"],
+        "fill-extrusion-color": "#ffffff", 
+        
         // Get `fill-extrusion-height` from the source `height` property.
         // 'fill-extrusion-height': ['get', 'height'],
-        "fill-extrusion-height": ["get", "height"],
+        //"fill-extrusion-height": ["get", "height"],
+        // "fill-extrusion-height": ['/',
+        // ['get', 'height', ['get', 'options']],
+        // 100],
+        //
+        "fill-extrusion-height": [
+          "+",
+          ["*", ["to-number", ["get", "levelIndex"]], 3],
+          ["/", ["get", "height", ["get", "options"]], 100]
+        ],
 
         // Get `fill-extrusion-base` from the source `base_height` property.
-        "fill-extrusion-base": 0,
+        "fill-extrusion-base": ["*", ["get", "levelIndex"], 300],
+        
 
         // Make extrusions slightly opaque to see through indoor walls.
         "fill-extrusion-opacity": 0.8,
+        //'line-width': ['get', ['get', 'options'], 'thickness']
+        "line-color": "#ED1C24",
       },
     });
     map.addControl(new mapboxgl.NavigationControl());
