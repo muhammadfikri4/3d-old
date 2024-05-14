fa8de2bc660171a8cfd77c8960b68bd5a4fffc35
Muhammad Fikri <unicornhorse1@gmail.com>
2024-05-14 22:53:49 +0700

    create trigger layer

--- src/hooks/Geo.js
+++ src/hooks/Geo.js
@@ -1,20 +1,19 @@
 import geojson from "../utils/data.json";
 
 export const useGeo = () => {
-	mapboxgl.accessToken =
-		"pk.eyJ1IjoicmFqaWZtYWhlbmRyYSIsImEiOiJjbHVjYTI2d2MwcnBzMmxxbndnMnNlNTUyIn0.aaCGYQ2OYIcIsAa4X-ILDA";
-	const map = new mapboxgl.Map({
-		// Choose from Mapbox's core styles, or make your own style with Mapbox Studio
-		style: "mapbox://styles/mapbox/light-v11",
-		center: [103.85266553640567,
-						1.2841309586464738],
-		zoom: 15.5,
-		pitch: 45,
-		bearing: -17.6,
-		container: "map",
-		antialias: true,
-	});
-	/*
+  mapboxgl.accessToken =
+    "pk.eyJ1IjoicmFqaWZtYWhlbmRyYSIsImEiOiJjbHVjYTI2d2MwcnBzMmxxbndnMnNlNTUyIn0.aaCGYQ2OYIcIsAa4X-ILDA";
+  const map = new mapboxgl.Map({
+    // Choose from Mapbox's core styles, or make your own style with Mapbox Studio
+    style: "mapbox://styles/mapbox/light-v11",
+    center: [103.85266553640567, 1.2841309586464738],
+    zoom: 15.5,
+    pitch: 45,
+    bearing: -17.6,
+    container: "map",
+    antialias: true,
+  });
+  /*
 	map.on("style.load", () => {
 		// Insert the layer beneath any symbol layer.
 		const layers = map.getStyle().layers;
@@ -71,31 +70,55 @@ export const useGeo = () => {
 		);
 	});
 	*/
-	map.on('load', () => {
-        map.addSource('floorplan', {
-            'type': 'geojson',
-            // 'data': 'http://localhost:3000/geojson/16-collyer-quay-mix.geojson'
-            'data': geojson
-        });
-        map.addLayer({
-            'id': 'room-extrusion',
-            'type': 'fill-extrusion',
-            'source': 'floorplan',
-            'paint': {
-                // Get the `fill-extrusion-color` from the source `color` property.
-                'fill-extrusion-color': ['get', 'color'],
-
-                // Get `fill-extrusion-height` from the source `height` property.
-                // 'fill-extrusion-height': ['get', 'height'],
-                'fill-extrusion-height': ['get', 'height'],
-
-                // Get `fill-extrusion-base` from the source `base_height` property.
-                'fill-extrusion-base': 0,
-
-                // Make extrusions slightly opaque to see through indoor walls.
-                'fill-extrusion-opacity': 0.8
-            }
-        });
-        map.addControl(new mapboxgl.NavigationControl());
+  map.on("load", () => {
+    map.addSource("floorplan", {
+      type: "geojson",
+      // 'data': 'http://localhost:3000/geojson/16-collyer-quay-mix.geojson'
+      data: geojson,
     });
+    map.addLayer({
+      id: "room-extrusion",
+      type: "fill-extrusion",
+      source: "floorplan",
+      paint: {
+        // Get the `fill-extrusion-color` from the source `color` property.
+        "fill-extrusion-color": ["get", "color"],
+
+        // Get `fill-extrusion-height` from the source `height` property.
+        // 'fill-extrusion-height': ['get', 'height'],
+        "fill-extrusion-height": ["get", "height"],
+
+        // Get `fill-extrusion-base` from the source `base_height` property.
+        "fill-extrusion-base": 0,
+
+        // Make extrusions slightly opaque to see through indoor walls.
+        "fill-extrusion-opacity": 0.8,
+      },
+    });
+    map.addControl(new mapboxgl.NavigationControl());
+
+    // Triger Layer
+    map.on("click", "room-extrusion", (e) => {
+      // Get the properties of the clicked feature
+      const properties = e.features[0].properties;
+      console.log("Triger Layer =>", properties);
+
+      // You can trigger other actions based on the clicked properties
+      // For example, showing a popup with room information
+      new mapboxgl.Popup()
+        .setLngLat(e.lngLat)
+        .setHTML(`<h3>${properties.name}</h3><p>${properties.type}</p>`)
+        .addTo(map);
+    });
+
+    // Change the cursor to a pointer when the mouse is over the room-extrusion layer
+    map.on("mouseenter", "room-extrusion", () => {
+      map.getCanvas().style.cursor = "pointer";
+    });
+
+    // Change the cursor back when it leaves the room-extrusion layer
+    map.on("mouseleave", "room-extrusion", () => {
+      map.getCanvas().style.cursor = "";
+    });
+  });
 };
